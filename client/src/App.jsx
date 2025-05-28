c244badf5cac750f7a9b9441d703ddcc64efb5b7
spider-shan <roshans20082004@gmail.com>
2025-05-28 15:48:42 +0530

    update app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import SignUp from './pages/SignUp';
+import Home from './pages/Home';
 function App() {
   return (
-    <h1>Hi<h1>
+   <Home></Home>
   )
 }
 
