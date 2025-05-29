3ac4da42340fc00a00cb45feacbb291ffc991237
spider-shan <roshans20082004@gmail.com>
2025-05-29 15:44:50 +0530

    addlayouts

---
+++ client/src/layouts/DashboardLayouts.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+function DashboardLayouts(props) {
+    return (
+        <div className="min-h-screen flex flex-col">
+            <header className="p-4 bg-blue-900 text-white">
+                <nav>
+                    <ul className="flex gap-6 list-none m-0 p-0 items-center">
+                        <li>
+                            <a href="/dashboard/intro" className="no-underline text-white hover:text-blue-300">Introduction</a>
+                        </li>
+                        <li>
+                            <a href="/dashboard/profile" className="no-underline text-white hover:text-blue-300">Profile</a>
+                        </li>
+                        <li>
+                            <a href="/dashboard/attendance" className="no-underline text-white hover:text-blue-300">Attendance</a>
+                        </li>
+                        <li>
+                            <a href="/dashboard/marksheet" className="no-underline text-white hover:text-blue-300">Marksheet</a>
+                        </li>
+                        <li className="ml-auto">
+                            <a
+                                href="/dashboard/logout"
+                                className="bg-red-600 hover:bg-red-500 text-white font-semibold py-2 px-4 rounded transition-colors duration-200 "
+                            >
+                                Logout
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+            <main className="flex-1 p-8">
+                {props.children}
+            </main>
+        </div>
+    );
+}
+
+export default DashboardLayouts;
